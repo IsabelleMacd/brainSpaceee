27a856ccc9cf32a82c7f06d7f2304a8964890834
Isabelle <isabellemacdonaldd@gmail.com>
2023-06-27 11:35:22 -0400

    Initial Commit

---
+++ brainSpaceee/brainSpaceeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainSpaceeeApp.swift
+//  brainSpaceee
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct brainSpaceeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
